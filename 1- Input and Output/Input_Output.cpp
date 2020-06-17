e29243b9e75f3c4ed0cd27b960733f0b65aba2e8
bhanuteja_g <g.bhanuteja23@gmail.com>
2020-06-17 05:35:41 +0530

    more changes

---
+++ 1- Input and Output/Input_Output.cpp
@@ -0,0 +1,27 @@
+// Q Link:-
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    int a,b,c,sum=0;
+
+      cin>>a>>b>>c;
+      sum=sum+a+b+c;    //To sum the three numbers
+
+    cout<<sum;
+
+    return 0;
+}
+
+
+
---
+++ 2- Basic Data Types/Basic_Data_Types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+
+    int x1;
+    long x2;
+    char x3;
+    float x4;
+    double x5;
+
+    scanf("%d %ld %c %f %lf",&x1, &x2, &x3, &x4, &x5 );
+
+                                      //Inputting variables of all data types in a
+                                      //single line
+
+
+    printf("%d\n%ld\n%c\n%f\n%lf", x1,x2,x3,x4,x5);
+
+                                                          //Priting each variable in
+                                                         //a new line
+    return 0;
+}
+
+
